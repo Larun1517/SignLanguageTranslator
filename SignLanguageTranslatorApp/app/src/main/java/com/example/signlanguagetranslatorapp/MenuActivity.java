40c47540ae838b97e269c99b58f144bcb6bd7146
PJD8013 <54406241+pjd8013@users.noreply.github.com>
2023-04-02 19:40:35 +0900

    STT액티비티 추가

--- SignLanguageTranslatorApp/app/src/main/java/com/example/signlanguagetranslatorapp/MenuActivity.java
+++ SignLanguageTranslatorApp/app/src/main/java/com/example/signlanguagetranslatorapp/MenuActivity.java
@@ -1,18 +1,18 @@
 package com.example.signlanguagetranslatorapp;
 
 /**
-    Licensed under the Apache License, Version 2.0 (the "License");
-    you may not use this file except in compliance with the License.
-    You may obtain a copy of the License at
+ Licensed under the Apache License, Version 2.0 (the "License");
+ you may not use this file except in compliance with the License.
+ You may obtain a copy of the License at
 
-            http://www.apache.org/licenses/LICENSE-2.0
+ http://www.apache.org/licenses/LICENSE-2.0
 
-    Unless required by applicable law or agreed to in writing, software
-    distributed under the License is distributed on an "AS IS" BASIS,
-    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-    See the License for the specific language governing permissions and
-    limitations under the License.
-*/
+ Unless required by applicable law or agreed to in writing, software
+ distributed under the License is distributed on an "AS IS" BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ See the License for the specific language governing permissions and
+ limitations under the License.
+ */
 
 import android.Manifest;
 import android.app.AlertDialog;
@@ -29,6 +29,7 @@ import androidx.core.content.ContextCompat;
 
 import android.view.Gravity;
 import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.LinearLayout;
 
 import java.io.DataInputStream;
@@ -92,6 +93,8 @@ public class MenuActivity extends AppCompatActivity {
         LinearLayout StartSTT = findViewById(R.id.start_button_stt);
         StartSTT.setOnClickListener( v -> {
             // STT 액티비티
+            Intent intent = new Intent(getApplicationContext(), STTActivity.class);
+            startActivity(intent);
         });
 
         // 어플리케이션 설정
